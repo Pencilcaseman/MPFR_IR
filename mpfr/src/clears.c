7ac6f8db4e8f70150ccfd65e661fcf081ad9023b
Toby Davis <pencilcaseman@gmail.com>
2022-07-21 18:51:45 +0100

    Might work on Linux now

--- mpfr/src/clears.c
+++ mpfr/src/clears.c
@@ -25,21 +25,12 @@ https://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
 # include "config.h"
 #endif
 
-#if HAVE_STDARG
 # include <stdarg.h>
-#else
-# include <varargs.h>
-#endif
 
 #include "mpfr-impl.h"
 
 void
-#if HAVE_STDARG
 mpfr_clears (mpfr_ptr x, ...)
-#else
-mpfr_clears (va_alist)
- va_dcl
-#endif
 {
   va_list arg;
 
